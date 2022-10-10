5fa1cd2b4e691067b98345362491d1090d357761
yu7400ki <yu7400ki@gmail.com>
2022-10-10 16:00:39 +0900

    Settingクラスの作成

--- src/atcoder_dumper/cli.py
+++ src/atcoder_dumper/cli.py
@@ -1,5 +1,6 @@
 import json
 import os
+from typing import Dict, List
 
 import git
 from fire import Fire
@@ -8,19 +9,32 @@ services = ["atcoder.jp"]
 settings_file = "settings.json"
 
 
+class Setting:
+    def __init__(
+        self,
+        service: str,
+        username: str = "",
+        filter: Dict[str, List[str]] = {"status": [], "filter": []},  # noqa
+    ):
+        self.service = service
+        self.username = username
+        self.filter = filter
+
+
+def _load_settings() -> Dict[str, Setting]:
+    with open(settings_file, mode="r", encoding="UTF-8") as file:
+        settings = json.load(file)
+
+    for service in settings:
+        settings[service] = Setting(**settings[service])
+
+    return settings
+
+
 def init() -> None:
     """Initialize the directory for the first time."""
 
-    init_settings = {
-        service: {
-            "username": "",
-            "filter": {
-                "status": [],
-                "language": [],
-            },
-        }
-        for service in services
-    }
+    init_settings = {service: Setting(service) for service in services}
 
     for d in services:
         if not os.path.isdir(d):
@@ -30,10 +44,10 @@ def init() -> None:
         with open(settings_file, mode="w", encoding="UTF-8") as file:
             for s in services:
                 username = input(f"Please enter your {s} username: ")
-                init_settings[s]["username"] = username
-            json.dump(init_settings, file, indent=4)
+                init_settings[s].username = username
+            json.dump({service: setting.__dict__ for service, setting in init_settings.items()}, file, indent=4)
 
-    if not os.path.exists(".git"):
+    if not os.path.isdir(".git"):
         repo = git.Repo.init()
         repo.git.add(settings_file)
         repo.index.commit("Initial commit")
