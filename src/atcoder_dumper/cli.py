cdb651aea604b1082715d8f81c5e7cfa6f702b03
yu7400ki <yu7400ki@gmail.com>
2022-10-10 22:35:36 +0900

    Filterクラスの作成

--- src/atcoder_dumper/cli.py
+++ src/atcoder_dumper/cli.py
@@ -11,43 +11,58 @@ services = ["atcoder.jp"]
 settings_file = "settings.json"
 
 
+class Filter:
+    def __init__(self, result: List[str] = [], language: List[str] = []) -> None:
+        self.result: List[str] = result
+        self.language: List[str] = language
+
+
 class Setting:
     def __init__(
         self,
         service: str,
-        username: str = "",
-        filter: Dict[str, List[str]] = {"result": [], "filter": []},  # noqa
+        username: str,
+        filter: Filter = Filter(),  # noqa
     ):
         self.service = service
         self.username = username
         self.filter = filter
 
 
+def _setting2json(setting: Setting) -> Dict[str, str | Dict[str, List[str]]]:
+    return {
+        "service": setting.service,
+        "username": setting.username,
+        "filter": {
+            "result": setting.filter.result,
+            "language": setting.filter.language,
+        },
+    }
+
+
 def _load_settings() -> Dict[str, Setting]:
     with open(settings_file, mode="r", encoding="UTF-8") as file:
         settings = json.load(file)
 
-    for service in settings:
-        settings[service] = Setting(**settings[service])
-
-    return settings
+    return {service: Setting(**setting) for service, setting in settings.items()}
 
 
 def init() -> None:
     """Initialize the directory for the first time."""
 
-    init_settings = {service: Setting(service) for service in services}
+    init_settings: Dict[str, Setting] = {}
 
-    for d in services:
-        if not os.path.isdir(d):
-            os.mkdir(d)
+    for service in services:
+        if not os.path.isdir(service):
+            os.mkdir(service)
 
     if not os.path.isfile(settings_file):
         with open(settings_file, mode="w", encoding="UTF-8") as file:
-            for s in services:
-                username = input(f"Please enter your {s} username: ")
-                init_settings[s].username = username
-            json.dump({service: setting.__dict__ for service, setting in init_settings.items()}, file, indent=4)
+            for service in services:
+                username = input(f"Please enter your {service} username: ")
+                init_settings[service] = Setting(service, username)
+
+            json.dump({service: _setting2json(setting) for service, setting in init_settings.items()}, file, indent=4)
 
     if not os.path.isdir(".git"):
         repo = git.Repo.init()
